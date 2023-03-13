7745dd522325c2241e3c2a3b86eae5c1ee59d918
dynamitedefender <111241345+dynamitedefender@users.noreply.github.com>
2023-03-13 18:47:59 +0000

    Add files via upload

---
+++ Organise.py
@@ -0,0 +1,19 @@
+import shutil
+import os
+fromdir = "Test"
+todir = "Test_1"
+list_of_files = os.listdir(fromdir)
+#print(list_of_files)
+for file_name in list_of_files:
+    name, extension = os.path.splitext(file_name)
+    #print(name)
+    #print(extension)
+    
+    if extension == "":
+        continue
+    
+    if os.path.exists(fromdir + "/" + extension):
+        shutil.move(fromdir + "/" + file_name, fromdir + "/" + extension + "/" + file_name)
+    else:
+        os.mkdir(fromdir + "/" + extension)
+        shutil.move(fromdir + "/" + file_name, fromdir + "/" + extension + "/" + file_name)
